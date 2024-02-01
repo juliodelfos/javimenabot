edbd43d6fb949af7bd0233fb33d34137ef7189c3
yerigagarin <yersonolivares@protonmail.com>
2024-02-01 10:55:34 +0000

    mejor código

--- index.js
+++ index.js
@@ -2,37 +2,34 @@ require("dotenv").config();
 const Twit = require("twit");
 const axios = require("axios").default;
 
-const largoArregloFrases = async () => {
-  const response = await axios.get(process.env.API_AIRTABLE);
-  const cantidadDeFrasesPosibles = response.data.records.length;
-  const randomNumber = (max) => Math.floor(Math.random() * (max - 0 + 1)) + 0;
-  return randomNumber(cantidadDeFrasesPosibles);
+const obtenerFraseAleatoria = async () => {
+  try {
+    const response = await axios.get(process.env.API_AIRTABLE);
+    const records = response.data.records;
+    const numeroRandom = Math.floor(Math.random() * records.length);
+    return records[numeroRandom].fields.tuit;
+  } catch (error) {
+    throw new Error("Error al obtener la frase aleatoria: " + error.message);
+  }
 };
 
-const fraseRandom = async () => {
-  const response = await axios.get(process.env.API_AIRTABLE);
-  const numeroRandom = await largoArregloFrases();
-  const frase = await response.data.records[numeroRandom].fields.tuit;
-  return frase;
+const publicarTuit = async () => {
+  try {
+    const frase = await obtenerFraseAleatoria();
+    const client = new Twit({
+      consumer_key: process.env.TWITTER_API_KEY,
+      consumer_secret: process.env.TWITTER_API_SECRET,
+      access_token: process.env.TWITTER_ACCESS_TOKEN,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    });
+    const tweet = { status: frase };
+    const { data } = await client.post("statuses/update", tweet);
+    console.log("Tweet publicado:", data.text);
+  } catch (error) {
+    console.error("Error al publicar el tuit:", error.message);
+  }
 };
-
-const client = new Twit({
-  consumer_key: process.env.TWITTER_API_KEY,
-  consumer_secret: process.env.TWITTER_API_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-});
 
 (async () => {
-  client.post(
-    "statuses/update",
-    { status: await fraseRandom() },
-    function (err, data, response) {
-      if (data) {
-        console.log(data);
-      } else if (err) {
-        console.log(err);
-      }
-    }
-  );
+  await publicarTuit();
 })();
